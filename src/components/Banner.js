13675da68a25a2c01ebdee26f370e368c25000bb
Jenny Schutzman <Jschutzman@stempremier.com>
2020-08-22 12:20:28 -0400

    finished picture uploads for homepage backgrounds.  attempted to /# for href fix.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,20 +1,27 @@
 import React from 'react'
 
-const Banner = (props) => (
-    <section id="banner" className="major">
-        <div className="inner">
-            <header className="major">
-                <h1>Jenny Schutzman, Software Engineer</h1>
-            </header>
-            <div className="content">
-                <p>Javascript, React, Redux, Angular, Rest APIs, NodeJS, Auth0, UI-Bootstrap, Material-UI Next, i18Next, MySQL, CouchDB/PouchDB, Git/Github, html, css, atom, IntelliJ, VSCode and more.  
-                </p>
-                <ul className="actions">
-                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
-                </ul>
-            </div>
-        </div>
-    </section>
+const Banner = props => (
+  <section id="banner" className="major">
+    <div className="inner">
+      <header className="major">
+        <h1>Jenny Schutzman, Software Engineer</h1>
+      </header>
+      <div className="content">
+        <p>
+          Javascript, React, Redux, Angular, Rest APIs, NodeJS, Auth0,
+          UI-Bootstrap, Material-UI Next, i18Next, MySQL, CouchDB/PouchDB,
+          Git/Github, html, css, atom, IntelliJ, VSCode and more.
+        </p>
+        <ul className="actions">
+          <li>
+            <a href="#one" className="button next scrolly">
+              Enter my resume site
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
 )
 
 export default Banner
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,90 +4,113 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import Banner from '../components/Banner'
 
+import books from '../assets/images/books.jpg'
 import cherryBlossom from '../assets/images/cherryBlossom.jpg'
 import columns from '../assets/images/columns.jpg'
-import irelandTrees from '../assets/images/irelandTrees.jpg'
+import irelandTownInWoods from '../assets/images/irelandTownInWoods.jpg'
+import parqueTyrona from '../assets/images/parqueTyrona.jpg'
+import theranchfields from '../assets/images/theranchfields.jpg'
+//import waterfall from '../assets/images/waterfall.jpg'
+
 import pic04 from '../assets/images/pic04.jpg'
 import pic05 from '../assets/images/pic05.jpg'
 import pic06 from '../assets/images/pic06.jpg'
 
 class HomeIndex extends React.Component {
-    render() {
+  render() {
+    return (
+      <Layout>
+        <Helmet
+          title="Jenny Schutzman - Software Engineer"
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' },
+          ]}
+        ></Helmet>
 
-        return (
-            <Layout>
-                <Helmet
-                    title="Gatsby Starter - Forty"
-                    meta={[
-                        { name: 'description', content: 'Sample' },
-                        { name: 'keywords', content: 'sample, something' },
-                    ]}
-                >
-                </Helmet>
+        <Banner />
 
-                <Banner />
-
-                <div id="main">
-                    <section id="one" className="tiles">
-                        <article style={{backgroundImage: `url(${cherryBlossom})`}}>
-                            <header className="major">
-                                <h3>About Me Now</h3>
-                                <p>Learn what it like to work with me</p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
-                        <article style={{backgroundImage: `url(${columns})`}}>
-                            <header className="major">
-                                <h3>My Other Life</h3>
-                                <p>Professional Experience Before software development</p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
-                        <article style={{backgroundImage: `url(${irelandTrees})`}}>
-                            <header className="major">
-                                <h3>Code Style</h3>
-                                <p>What I love about software development and my coding philospies</p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
-                        <article style={{backgroundImage: `url(${pic04})`}}>
-                            <header className="major">
-                                <h3>Software Experience</h3>
-                                <p>Come check out my stuff</p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
-                        <article style={{backgroundImage: `url(${pic05})`}}>
-                            <header className="major">
-                                <h3>CHS Women Devs</h3>
-                                <p>Learn more about my group for women developers in the Charleston Area</p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
-                        <article style={{backgroundImage: `url(${pic06})`}}>
-                            <header className="major">
-                                <h3>Mentoring Program</h3>
-                                <p>Check out upcoming activities Women in Tech Mentoring Program 2019-2020 and join us!</p>
-                            </header>
-                            <Link to="/landing" className="link primary"></Link>
-                        </article>
-                    </section>
-                    <section id="two">
-                        <div className="inner">
-                            <header className="major">
-                                <h2>Massa libero</h2>
-                            </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.</p>
-                            <ul className="actions">
-                                <li><Link to="/landing" className="button next">Get Started</Link></li>
-                            </ul>
-                        </div>
-                    </section>
-                </div>
-
-            </Layout>
-        )
-    }
+        <div id="main">
+          <section id="one" className="tiles">
+            <article style={{ backgroundImage: `url(${cherryBlossom})` }}>
+              <header className="major">
+                <h3>My Other Life</h3>
+                <p>Professional Experience Before software development</p>
+              </header>
+              <Link to="/landing" className="link primary"></Link>
+            </article>
+            <article style={{ backgroundImage: `url(${columns})` }}>
+              <header className="major">
+                <h3>About Me Now</h3>
+                <p>Learn who I am and what it like to work with me</p>
+              </header>
+              <Link to="/landing" className="link primary"></Link>
+            </article>
+            <article style={{ backgroundImage: `url(${theranchfields})` }}>
+              <header className="major">
+                <h3>My Code</h3>
+                <p>Come check out some of my stuff</p>
+              </header>
+              <Link to="/landing" className="link primary"></Link>
+            </article>
+            <article style={{ backgroundImage: `url(${books})` }}>
+              <header className="major">
+                <h3>Code Style</h3>
+                <p>
+                  What I love about software development and my coding
+                  philospies
+                </p>
+              </header>
+              <Link to="/landing" className="link primary"></Link>
+            </article>
+            <article style={{ backgroundImage: `url(${parqueTyrona})` }}>
+              <header className="major">
+                <h3>Women in Tech Mentoring Program</h3>
+                <p>
+                  Check out upcoming activities Women in Tech Mentoring Program
+                  and join us!
+                </p>
+              </header>
+              <Link to="/landing" className="link primary"></Link>
+            </article>
+            <article style={{ backgroundImage: `url(${irelandTownInWoods})` }}>
+              <header className="major">
+                <h3>CHS Women Devs</h3>
+                <p>
+                  Learn more about my group for women developers in the
+                  Charleston Area
+                </p>
+              </header>
+              <Link to="/landing" className="link primary"></Link>
+            </article>
+          </section>
+          <section id="two">
+            <div className="inner">
+              <header className="major">
+                <h2>Massa libero</h2>
+              </header>
+              <p>
+                Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
+                libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
+                Pellentesque condimentum sem. In efficitur ligula tate urna.
+                Maecenas laoreet massa vel lacinia pellentesque lorem ipsum
+                dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et
+                sagittis libero. Mauris aliquet magna magna sed nunc rhoncus
+                amet pharetra et feugiat tempus.
+              </p>
+              <ul className="actions">
+                <li>
+                  <Link to="/landing" className="button next">
+                    Get Started
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </section>
+        </div>
+      </Layout>
+    )
+  }
 }
 
 export default HomeIndex
